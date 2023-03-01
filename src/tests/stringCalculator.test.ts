3162ee70bf2c52c2d6051c1680f9a45296b1234d
Baenas <dbg94dani@gmail.com>
2023-03-01 19:11:46 +0100

    RED: Le pasamos UNO y devuelve UNO

--- src/tests/stringCalculator.test.ts
+++ src/tests/stringCalculator.test.ts
@@ -10,3 +10,14 @@ test('No le pasamos nada devuelve 0', () => {
     expect(value).toBe(0);
 
 });
+
+test('Le pasamos UNO y devuelve UNO', () => {
+    
+    let string = "1";
+    let stringOperator = new stringCalculator();
+
+    let value = stringOperator.add(string);
+
+    expect(value).toBe(1);
+
+});
