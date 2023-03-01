7f2bdc60e75bbdb0ea3ad0fe3b7350c1a94ef276
Baenas <dbg94dani@gmail.com>
2023-03-01 19:27:50 +0100

    GREEN:  Le pasamos dos numeros separados por comas

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,7 +1,15 @@
 export class stringCalculator {
 
-    add(string : string) {
-        return  Number(string) 
+    add(string: string) {
+
+        let numbers = string.split(',')
+
+        if (numbers.length === 2) {
+            let total = Number(numbers[0]) + Number(numbers[1]);
+            return total
+        }
+
+        return Number(string)
 
     }
 
